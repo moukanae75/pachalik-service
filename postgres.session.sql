623b1d69473543b5f87b1d49c92d58f5b1ee6b55
moukanae75 <moukanae75@gmail.com>
2026-08-02 14:32:39 +0100

    chore: configure SQLTools connection settings and remove redundant stamps from document requirements list

--- postgres.session.sql
+++ postgres.session.sql
@@ -36,8 +36,8 @@ INSERT INTO document (service_id, document) VALUES
 (1, 'Quatre photos'),
 (1, 'Ancien passeport (si disponible)'),
 (1, 'Timbre de 500 DH'),
-(1, 'Timbre de 600 DH'),
-(1, 'Timbre de 700 DH'),
+
+
 
 (2, 'Autorisation parentale accompagnée d''une copie de la CNI du père et de la mère'),
 (2, 'Quatre photos'),
